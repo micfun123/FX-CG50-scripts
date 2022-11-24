894599611df684c501833909cc77823e7af91734
Michael Parker <54761507+micfun123@users.noreply.github.com>
2022-11-24 13:15:25 +0000

    Add files via upload

--- circle.py
+++ circle.py
@@ -6,6 +6,7 @@ y2 = float(input("Enter the value of y2: "))
 center = ((xi + x2) / 2, (yi + y2) / 2)
 radiusfromcenter = ((x2 - xi) ** 2 + (y2 - yi) ** 2) ** 0.5
 area = 3.14159 * radiusfromcenter ** 2
+areainpi = area / 3.14159
 gradient = (y2 - yi) / (x2 - xi)
 equation = "y = " + str(gradient) + "x + " + str(yi - gradient * xi)
 bisector = "y = " + str(-1 / gradient) + "x + " + str(yi - (-1 / gradient) * xi)
@@ -13,5 +14,6 @@ bisector = "y = " + str(-1 / gradient) + "x + " + str(yi - (-1 / gradient) * xi)
 print("Center: ({:.2f}, {:.2f})".format(center[0], center[1]))
 print("Radius: {:.2f}".format(radiusfromcenter))
 print("Area: {:.2f}".format(area))
+print("Area in pi: {:.2f}".format(areainpi))
 print("Equation: " + equation)
 print("Bisector: " + bisector)
