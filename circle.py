9b0a8dddcffe235a41e561fd46e42a427846aa08
Michael Parker <54761507+micfun123@users.noreply.github.com>
2022-11-24 13:14:13 +0000

    Add files via upload

--- circle.py
+++ circle.py
@@ -1,12 +1,17 @@
-xi = int(input("Enter the value of x1: "))  
-yi = int(input("Enter the value of y1: "))
-x2 = int(input("Enter the value of x2: "))
-y2 = int(input("Enter the value of y2: "))
+xi = float(input("Enter the value of x1: "))  
+yi = float(input("Enter the value of y1: "))
+x2 = float(input("Enter the value of x2: "))
+y2 = float(input("Enter the value of y2: "))
 
-print("center are: ", (xi+x2)/2, (yi+y2)/2)
-print("radius: ", ((x2-xi)**2 + (y2-yi)**2)**0.5)
-print("area: ", 3.14*((x2-xi)**2 + (y2-yi)**2)**0.5)
-print("gradient: ", (y2-yi)/(x2-xi))
-print("equation: ", "(x-", (xi+x2)/2, ")^2 + (y-", (yi+y2)/2, ")^2 = ", ((x2-xi)**2 + (y2-yi)**2)**0.5)
-print("bisector: ", "y = ", (y2-yi)/(x2-xi), "x + ", ((y2-yi)/(x2-xi))*((xi+x2)/2) - ((yi+y2)/2))
-
+center = ((xi + x2) / 2, (yi + y2) / 2)
+radiusfromcenter = ((x2 - xi) ** 2 + (y2 - yi) ** 2) ** 0.5
+area = 3.14159 * radiusfromcenter ** 2
+gradient = (y2 - yi) / (x2 - xi)
+equation = "y = " + str(gradient) + "x + " + str(yi - gradient * xi)
+bisector = "y = " + str(-1 / gradient) + "x + " + str(yi - (-1 / gradient) * xi)
+#print to 2 decimal places
+print("Center: ({:.2f}, {:.2f})".format(center[0], center[1]))
+print("Radius: {:.2f}".format(radiusfromcenter))
+print("Area: {:.2f}".format(area))
+print("Equation: " + equation)
+print("Bisector: " + bisector)
